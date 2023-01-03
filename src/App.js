229511efcd035a92bb2c57236ac3520a94636201
CatLouise3 <mcfeec@hotmail.ca>
2023-01-03 06:54:59 -0500

    added github link

--- src/App.js
+++ src/App.js
@@ -11,10 +11,28 @@ export default function App() {
     <div className="App">
       <Header />
       <Search />
-
       <City city="Montreal" />
       <Weather temperature={9} />
       <Forecast />
+      <a
+        href="https://github.com/CatLouise3/weather-react"
+        class="git"
+        target="_blank"
+        rel="noreferrer"
+      >
+        {" "}
+        GitHub{" "}
+      </a>{" "}
+      by{" "}
+      <a
+        href="https://www.linkedin.com/in/catherine-mcfee-b96899126/"
+        class="linkedIn"
+        target="_blank"
+        rel="noreferrer"
+      >
+        {" "}
+        Catherine
+      </a>
     </div>
   );
 }
