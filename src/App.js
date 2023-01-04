57f083e40c3d7d48e4ad8cd8aa8aef7d17915253
CatLouise3 <mcfeec@hotmail.ca>
2023-01-03 19:31:32 -0500

    added Git

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Search from "./Search";
 import City from "./City";
 import Weather from "./Weather";
 import Forecast from "./Forecast";
+import Git from "./Git";
+
 export default function App() {
   return (
     <div className="App">
@@ -14,25 +16,7 @@ export default function App() {
       <City city="Montreal" />
       <Weather temperature={9} />
       <Forecast />
-      <a
-        href="https://github.com/CatLouise3/weather-react"
-        class="git"
-        target="_blank"
-        rel="noreferrer"
-      >
-        {" "}
-        GitHub{" "}
-      </a>{" "}
-      by{" "}
-      <a
-        href="https://www.linkedin.com/in/catherine-mcfee-b96899126/"
-        class="linkedIn"
-        target="_blank"
-        rel="noreferrer"
-      >
-        {" "}
-        Cat
-      </a>
+      <Git />
     </div>
   );
 }
---
+++ src/Git.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function GitLink() {
+  return (
+    <div>
+      <a
+        href="https://github.com/CatLouise3/weather-react"
+        class="git"
+        target="_blank"
+        rel="noreferrer"
+      >
+        {" "}
+        GitHub{" "}
+      </a>{" "}
+      by{" "}
+      <a
+        href="https://www.linkedin.com/in/catherine-mcfee-b96899126/"
+        class="linkedIn"
+        target="_blank"
+        rel="noreferrer"
+      >
+        {" "}
+        Catherine
+      </a>
+    </div>
+  );
+}
